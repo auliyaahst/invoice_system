99bd46bcb154ec01a368acbcc71a00cb2de50929
vools <auliyaav@gmail.com>
2024-11-06 17:39:22 +0700

    fix#7

--- frontend/deploy.js
+++
@@ -1,4 +0,0 @@
-const ghpages = require('gh-pages');
-
-// Adjust the 'build' directory if your build output is located elsewhere
-ghpages.publish('build', { src: ['**/*', '!node_modules/**/*', '!backend/**/*'] }, callback);
